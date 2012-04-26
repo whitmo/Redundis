2aca17d426986d11c3210fda50bb6f1bf575af65
Whit Morriss <whit@surveymonkey.com>
2012-04-25 20:21:27 -0500

    passing dl tests

--- redundis/tests/test_dl.py
+++ redundis/tests/test_dl.py
@@ -17,13 +17,40 @@ class TestDL(unittest.TestCase):
         from redundis import utils
         return utils.Downloader(cache=cache)
 
-
     def test_downloader(self):
         dl = self.makeone()
         with patch.object(dl, 'GET') as get:
             get.return_value = Mock(name='response')
-            get.return_value.content = self.fake.text()
+            get.return_value.content = self.fake.bytes()
             out = dl.download(self.url)
             
         assert out.exists()
+        assert out.bytes() == self.fake.bytes()
+
+    def place_file(self, dl):
+        thefile = dl.cache / dl.quote(self.url)
+        thefile.write_text(self.fake.bytes())
+        return thefile
+
+    def test_cache_hit(self):
+        dl = self.makeone()
+        self.place_file(dl)
+        out = dl.download(self.url)                
+        assert out.exists()
         assert out.text() == self.fake.text()
+
+    def test_cache_creation(self):
+        newcache = self.cache / 'new'
+        self.makeone(cache=newcache)
+        assert newcache.exists()
+
+    def test_unpack_to(self):
+        dl = self.makeone()
+        thefile = self.place_file(dl)
+        dest = self.cache / 'out-test'
+        out = dl.unpack_to(thefile, dest)
+        assert out == self.cache / 'out-test'
+        assert out.exists()
+        assert out.isdir()
+        assert (out / 'README').exists()
+
