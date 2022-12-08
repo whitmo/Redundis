4a4e71db54e5561f0de9df6dba6180d9f9d0e951
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 13:56:34 +0000

    Adding tarfile member sanitization to extractall()

--- redundis/utils.py
+++ redundis/utils.py
@@ -46,7 +46,26 @@ class Downloader(object):
             return dest
         tmp = path(self.tempdir())
         with tarfile.open(thefile, mode='r:gz') as archive:
-            archive.extractall(tmp)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, tmp)
 
         if len(tmp.dirs()) == 1:
             tmp.dirs()[0].copytree(dest)
